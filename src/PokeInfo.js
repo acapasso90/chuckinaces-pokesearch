0b864686f704b7984c0a077060e77a2204ea7547
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-02-19 13:29:03 -0500

    created pokesearch app for weight and height
    created pokesearch app for weight and height

---
+++ src/PokeInfo.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function PokeInfo(props){
+    const official = "official-artwork";
+    let loaded = props.loading;
+    let pokePicture = props.data.sprites.other.[official].front_default;
+    let pokeName = props.data.name;
+
+    // Gets weight in hectograms from API
+    let unformattedWeight = props.data.weight;
+    // Converts to lbs by dividing by 4.536
+    let formattedWeight = (unformattedWeight / 4.536);
+       // Rounds converted weight to nearest tenth
+       let roundedFormattedWeight = Math.round(10*formattedWeight)/10;
+    // Converts to kg by dividing by 10
+    let metricWeight = (unformattedWeight / 10);
+    // Gets height in decimeters
+    let unformattedHeight = props.data.height;
+    //Converts to feet by diving by 3.048
+    let formattedHeight = (unformattedHeight / 3.048);
+    // Rounds converted height to nearest tenth
+    let roundedFormattedHeight = Math.round(10*formattedHeight)/10;
+    // Converts to meters by dividing by 10
+    let metricHeight = (unformattedHeight / 10);
+console.log(props.data.height);
+
+if (loaded === "loaded"){return(
+    <div className="PokeInfo">
+        <div className="row">
+            <div className="column">
+        <img src={pokePicture} alt="pokemon" />
+       <h1 className="pokeName">{pokeName} </h1> 
+        </div>
+        <div className="column">
+    <h1>Weight: {roundedFormattedWeight}lbs / {metricWeight}kg </h1>
+    <h1>Height: {roundedFormattedHeight}ft / {metricHeight}m</h1>
+        </div>
+        </div>
+    </div>
+)}
+else {
+    return("loading")}
+
+}
---
+++ src/PokeSearch.js
@@ -0,0 +1,55 @@
+import React, {useState} from "react"
+import axios from "axios";
+import loading from "./loading.gif";
+import PokeInfo from "./PokeInfo.js";
+
+export default function PokeSearch(){
+const [pokemon, Setpokemon] = useState("spoink");
+const [pokeinfo, setPokeinfo] = useState("");
+const [loaded, setLoaded] = useState(false);
+const [loadedStatus, setLoadedStatus] = useState(" ");
+
+// sets Pokemon info and sets loaded status for PokeInfo.js
+function setInfo(response){
+    setPokeinfo(response.data)
+setLoaded(true);
+setLoadedStatus("loaded");
+}
+
+
+// prevents page refreshing and searches pokemon with pokemon set in setPokemon
+function handleSubmit(event){  event.preventDefault();
+   searchPokemon();
+}
+
+// on text input pokemon is changed to what is typed
+function setPokemon(event){
+    event.preventDefault();
+   const pokemonLowercase = (event.target.value).toLowerCase();
+    Setpokemon(pokemonLowercase);
+}
+
+// Searches pokemon and makes axios request to setInfo
+function searchPokemon () {    const APIurl = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+axios.get(APIurl).then(setInfo);}
+
+
+// once loaded shows input forms and displays PokeInfo from default search
+if(loaded){return(
+        <div className="PokeSearch" key={pokeinfo}>
+    <form onSubmit={handleSubmit}>
+    <input type="text" onChange={setPokemon} placeholder="Type a Pokémon Name" 
+    className="searchBar" />
+    <input type="submit" placeholder="Submit" className="submitButton" />
+    </form>
+    <PokeInfo data={pokeinfo} loading={loadedStatus} />
+        </div>)}
+//  searches default pokemon and shows loading pokeball gif
+else{ searchPokemon();
+    return(
+<div className="loading">
+    <img src={loading} alt="loading" />
+</div>
+
+)}
+}
