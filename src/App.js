9044d91e2e7a76a747481946faed63655aac5f3a
acapasso <69538713+acapasso90@users.noreply.github.com>
2021-03-13 13:33:18 -0500

    Removed overflow hidden from app, cleaned up code on anything involving prices
    Removed overflow hidden from app, cleaned and rewrote code involving prices to better position prices within cardInfo

--- src/App.js
+++ src/App.js
@@ -23,22 +23,22 @@ function App() {
      <DropdownButton id='dropdown-button-drop-down' className="regDropDown" title={dropdownTitle}>
      <div className="dropdownColumn">
         <NavLink to="/cards.name" style={{ textDecoration: 'none' }} className="dropdownLink"  href="#/action-1">  By Name </NavLink> <br/>
-        <NavLink to="/cards.id" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By ID </NavLink> <br/>
         <NavLink to="/cards.set" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By Set </NavLink> <br/>
         <NavLink to="/cards.series" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By Series </NavLink><br/>
         <NavLink to="/cards.subtype" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By SubType </NavLink>  <br/>
-        <NavLink to="/cards.rarity" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By Rarity </NavLink>
+        <NavLink to="/cards.rarity" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By Rarity </NavLink>  <br/>
+        <NavLink to="/cards.id" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By ID </NavLink>
   </div>
   </DropdownButton>
   <DropdownButton id='dropdown-button-drop-down' className="hamburgerDropdown" title={hamburger}>
      <div className="dropdownColumn">
      <NavLink to="/" style={{ textDecoration: 'none' }} className="dropdownLink"  href="#/action-1"> Stats </NavLink> <br/>
         <NavLink to="/cards.name" style={{ textDecoration: 'none' }} className="dropdownLink"  href="#/action-1"> Cards By Name </NavLink> <br/>
-        <NavLink to="/cards.id" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2">Cards By ID </NavLink> <br/>
         <NavLink to="/cards.set" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2">Cards By Set </NavLink> <br/>
         <NavLink to="/cards.series" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2">Cards By Series </NavLink> <br/>
         <NavLink to="/cards.subtype" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2">Cards By SubType </NavLink> <br/>
-        <NavLink to="/cards.rarity" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By Rarity </NavLink>
+        <NavLink to="/cards.rarity" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2"> By Rarity </NavLink>  <br/>
+        <NavLink to="/cards.id" style={{ textDecoration: 'none' }} className="dropdownLink" id="dropdownLinkTwo" href="#/action-2">Cards By ID </NavLink>
       <br />  <a href="https://www.twitch.tv/chuckinaces" target="_blank" className="dropdownLink" id="dropdownLinkTwo" >Twitch</a>
   </div>
 </DropdownButton>
